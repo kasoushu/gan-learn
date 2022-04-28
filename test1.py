595438529d3ac36f6c57014248779dffe5ee5644
kasoushu <kasoushu@gmail.com>
2022-04-28 09:17:44 +0800

    4/11/15:06

--- test1.py
+++ test1.py
@@ -1,5 +1,16 @@
-import matplotlib.pyplot as plt
 
-d = [x for x in range(10)]
-plt.plot(d,[k*2+1 for k in d])
-plt.show()
+from clint.textui import progress
+import requests
+
+def get_data_from_url(dataset:str):
+
+    horse2zebra_url = 'https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip'
+    res = requests.get(horse2zebra_url,stream=True)
+    total_length = int(res.headers.get('content-length'))
+    # print("123")
+    with open("hz.zip", "wb") as pypkg:
+        for chunk in progress.bar(res.iter_content(chunk_size=1024*1024), expected_size=(total_length/(1024*1024) ) + 1, width=100):
+            if chunk:
+                # print("write i")
+                pypkg.write(chunk)
+get_data_from_url('horse2zebra')
